c64057b7af0603a415c35d47d54fe7a01c875f02
Baljan Kanatbek <baljann19@gmail.com>
2025-09-11 07:45:11 +0300

    feat: enhance Jest configuration and update dependencies
    - Integrated `next/jest` for improved Jest configuration, allowing for Next.js compatibility.
    - Updated `jest.config.ts` to include a custom module name mapper for easier imports.
    - Added `jest-environment-jsdom` and `ts-node` as development dependencies in `package.json`.
    - Reflected these changes in `package-lock.json` to ensure consistent dependency management.

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,12 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -92,7 +97,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/components/$1",
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -198,4 +205,5 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
